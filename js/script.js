c23e6ce0a9676437a484f879bd986c4317854eb1
Shai Sussman <shai.sussman@gmail.com>
2023-07-28 09:28:24 +0300

    Basic buffer implementation

--- js/script.js
+++ js/script.js
@@ -6,8 +6,16 @@ var centerPoint = [35, 32.8]
 // Hide sldier element
 document.getElementById("overlay").style.display = "none"
 
+const polySym = {
+    type: "simple-fill", // autocasts as new SimpleFillSymbol()
+    color: [140, 0, 0, 0.25],
+    outline: {
+      color: [0, 0, 0, 0.5],
+      width: 2
+    }
+  };
 //Init map view
-require(["esri/config", "esri/Map", "esri/views/MapView", "esri/widgets/Locate"], 
+require(["esri/config", "esri/Map", "esri/views/MapView", "esri/widgets/Locate", "esri/geometry/Point", "esri/layers/GraphicsLayer", "esri/Graphic", "esri/geometry/geometryEngine"],
 (esriConfig, Map, MapView, Locate) => {
 
     esriConfig.apiKey = API_KEY;
@@ -23,26 +31,41 @@ require(["esri/config", "esri/Map", "esri/views/MapView", "esri/widgets/Locate"]
         container: "map" // Div element
     });
 
-    // Define location variable
-    const locateBtn = new Locate({
-        view: view
-    });
+        // Add the locate widget to the top left corner of the view
+        view.ui.add(locateBtn, {
+            position: "top-left"
+        });
 
-    // Add the locate widget to the top left corner of the view
-    view.ui.add(locateBtn, {
-        position: "top-left"
-    });
+        locateBtn.on("locate", ({ position }) => {
+            document.getElementById("overlay").style.display = "block"
+            doSomething(position)
 
-    locateBtn.on("locate", ({ position }) => {
-        document.getElementById("overlay").style.display = "block"
-        doSomething(position)
-        
-      })
-});
+        })
 
-function doSomething(location)
-{
-    const { longitude, latitude } = location.coords;
-        console.log(`lat: ${latitude.toFixed(4)}, long: ${longitude.toFixed(4)}`);
+        // Add buffer layer
+        const bufferLayer = new GraphicsLayer();
+        map.add(bufferLayer)
 
-}
+        function doSomething(location) {
+
+            const { longitude, latitude } = location.coords;
+            //console.log(`lat: ${latitude.toFixed(4)}, long: ${longitude.toFixed(4)}`);
+            var point = new Point({
+                x: longitude,
+                y: latitude,
+              });
+
+            const buffer = geometryEngine.geodesicBuffer(point, 560, "meters");
+            if(bufferLayer.graphics.length === 0){
+                bufferLayer.add(
+                  new Graphic({
+                    geometry: buffer,
+                    symbol: polySym
+                  })
+                );
+              } else {
+                const graphic = bufferLayer.graphics.getItemAt(0);
+                graphic.geometry = buffer;
+              }
+
+        }
