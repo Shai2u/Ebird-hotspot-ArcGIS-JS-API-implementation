cb89356bff74b241169d447aff4908f39e028920
Shai Sussman <shai.sussman@gmail.com>
2023-07-28 08:10:55 +0300

    Refactor whitespace

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ require(["esri/config", "esri/Map", "esri/views/MapView", "esri/widgets/Locate"]
     esriConfig.apiKey = "AAPK2b9d83399ba1413f8c6cff164ec8fd85-8M2RQwo1ZczBqrcqlwhQQ6jugWmzIwcIdU5-j23AMr5hU5MmA3SJeHl-U7-jFcJ";
 
     const map = new Map({
-      basemap: "arcgis-topographic" // Basemap layer service
+        basemap: "arcgis-topographic" // Basemap layer service
     });
 
     const view = new MapView({
@@ -14,7 +14,7 @@ require(["esri/config", "esri/Map", "esri/views/MapView", "esri/widgets/Locate"]
         center: [35, 32.8], // Longitude, latitude
         zoom: 13, // Zoom level
         container: "map" // Div element
-      });
+    });
 
     // Define location variable
     const locateBtn = new Locate({
