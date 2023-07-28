11a98400eff83682468bc22b94a1ae2f40934880
Shai Sussman <shai.sussman@gmail.com>
2023-07-28 08:37:57 +0300

    Convert hard-coded to variables

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
+
+// Define variables
+var API_KEY = "AAPK2b9d83399ba1413f8c6cff164ec8fd85-8M2RQwo1ZczBqrcqlwhQQ6jugWmzIwcIdU5-j23AMr5hU5MmA3SJeHl-U7-jFcJ"
+var centerPoint = [35, 32.8]
+
 // Hide sldier element
 document.getElementById("overlay").style.display = "none"
 
@@ -5,7 +10,7 @@ document.getElementById("overlay").style.display = "none"
 require(["esri/config", "esri/Map", "esri/views/MapView", "esri/widgets/Locate"], 
 (esriConfig, Map, MapView, Locate) => {
 
-    esriConfig.apiKey = "AAPK2b9d83399ba1413f8c6cff164ec8fd85-8M2RQwo1ZczBqrcqlwhQQ6jugWmzIwcIdU5-j23AMr5hU5MmA3SJeHl-U7-jFcJ";
+    esriConfig.apiKey = API_KEY;
 
     const map = new Map({
         basemap: "arcgis-topographic" // Basemap layer service
@@ -13,7 +18,7 @@ require(["esri/config", "esri/Map", "esri/views/MapView", "esri/widgets/Locate"]
 
     const view = new MapView({
         map: map,
-        center: [35, 32.8], // Longitude, latitude
+        center: centerPoint, // Longitude, latitude
         zoom: 13, // Zoom level
         container: "map" // Div element
     });
